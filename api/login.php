5da8cb416f337d60b5ef01339eafe428e3fbd867
Antoni March Marti <marchmarti.antoni@gmail.com>
2020-01-17 17:21:08 +0100

    chenge direction of folders

--- api/login.php
+++ api/login.php
@@ -27,10 +27,10 @@ $email_exists = $user->emailExists();
 // files for jwt will be here
 // generate json web token
 include_once 'config/core.php';
-include_once 'libs/php-jwt-master/src/BeforeValidException.php';
-include_once 'libs/php-jwt-master/src/ExpiredException.php';
-include_once 'libs/php-jwt-master/src/SignatureInvalidException.php';
-include_once 'libs/php-jwt-master/src/JWT.php';
+include_once 'libs/src/BeforeValidException.php';
+include_once 'libs/src/ExpiredException.php';
+include_once 'libs/src/SignatureInvalidException.php';
+include_once 'libs/src/JWT.php';
 use \Firebase\JWT\JWT;
  
 // check if email exists and if password is correct
